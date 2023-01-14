88889e8168cd1b14b55b73efb3e7ff96b6324039
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-01-14 15:46:43 +0000

    :sparkles::construction: initial directory file search

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::vec::Vec;
+use std::fs;
+use std::path::Path;
 
 
 struct AnimationCell {
@@ -9,10 +11,31 @@ struct AnimationCell {
 }
 
 fn search(directory: &str, sheet_name: &str) -> HashMap<String, Vec<AnimationCell>> {
+    let entries = recursive_search(directory.to_string());
+    print!("{:?}", entries);
     HashMap::new()
 }
 
+fn recursive_search(path_str: String) -> Vec<String> {
+    let path = Path::new(&path_str);
+    if path.is_dir() {
+        fs::read_dir(path)
+        .expect("Failed to load")
+        .flat_map(|res| 
+        recursive_search(
+            res
+            .expect("aaah")
+            .path()
+            .to_str()
+            .expect("failed")
+            .to_string()
+        ))
+        .collect::<Vec<String>>()
+    } else {
+        vec![path_str]
+    }
+}
+
 fn main() {
-    let hash_map = search("/hello/world", "my_sheet");
-    println!("Hello, world!");
+    search("./sample", "hello");
 }
