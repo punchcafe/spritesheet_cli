fe7b15702eb820ec5ce67d307a82bfc0e36caa28
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-01-14 14:35:42 +0000

    :sparkles::construction: initial search function contract

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+use std::vec::Vec;
+
+
+struct AnimationCell {
+    sprite_sheet_name: String,
+    animation_name: String,
+    cell_number: u8
+}
+
+fn search(directory: &str, sheet_name: &str) -> HashMap<String, Vec<AnimationCell>> {
+    HashMap::new()
+}
+
 fn main() {
+    let hash_map = search("/hello/world", "my_sheet");
     println!("Hello, world!");
 }
