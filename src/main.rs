98f5d8c5651a2625dce908e697463b7cd2ff087f
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-01-15 17:58:47 +0000

    :sparkles: add file matching regex

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use std::fs;
 use std::path::Path;
+use regex::Regex;
 
 
 struct AnimationCell {
@@ -11,31 +12,43 @@ struct AnimationCell {
 }
 
 fn search(directory: &str, sheet_name: &str) -> HashMap<String, Vec<AnimationCell>> {
-    let entries = recursive_search(directory.to_string());
+    let pattern = regex_expression(sheet_name);
+    let entries = recursive_search(directory.to_string(), &pattern);
     print!("{:?}", entries);
     HashMap::new()
 }
 
-fn recursive_search(path_str: String) -> Vec<String> {
+fn recursive_search(path_str: String, valid_file_pattern: &Regex) -> Vec<String> {
     let path = Path::new(&path_str);
     if path.is_dir() {
-        fs::read_dir(path)
+        fs::read_dir(&path)
         .expect("Failed to load")
         .flat_map(|res| 
         recursive_search(
             res
-            .expect("aaah")
+            .expect("unexpected error")
             .path()
             .to_str()
-            .expect("failed")
-            .to_string()
+            .expect("unexpected error")
+            .to_string(),
+            valid_file_pattern
         ))
         .collect::<Vec<String>>()
     } else {
-        vec![path_str]
+        if valid_file_pattern.is_match(&path_str) {
+            vec![path_str]
+        } else {
+            vec![]
+        }
     }
 }
 
+fn regex_expression(sheet_name: &str) -> Regex {
+    let pattern = format!("{sheet_name}\\.([^\\.])+\\.(\\d)+\\.(png)");
+    println!("pattern: {pattern}");
+    Regex::new(&pattern[..]).unwrap()
+}
+
 fn main() {
-    search("./sample", "hello");
+    search("./sample", "skater_base");
 }
